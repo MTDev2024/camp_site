8bd035ee0ee29fc2766384fc36bc10418ff5302f
M <mt.dev.2023@gmail.com>
2024-12-02 18:18:44 +0100

    form script modified

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function openNav() {
 function closeNav() {
     document.getElementById("mobile-nav").style.width = "0";
 }
+
+function validateForm() {
+    const destination = document.getElementById("destination").value;
+    const date = document.getElementById("date").value;
+    const people = document.getElementById("people").value;
+}
+if (!destination || !date || !people) {
+    alert ("Please fill in all the fields.");
+} else {
+    showModal();
+}
+
